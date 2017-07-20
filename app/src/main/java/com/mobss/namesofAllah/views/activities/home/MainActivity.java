e395eb727f6511f17173a91c4d0a81a8f6812f27
ilkayaktas <ilkayaktas@gmail.com>
2017-07-21 00:05:15 +0300

    RateMe is added. Turkish and english languages' strings are added.  Main activity onStart onStop onDestroy methods are implemented.

--- app/src/main/java/com/mobss/namesofAllah/views/activities/home/MainActivity.java
+++ app/src/main/java/com/mobss/namesofAllah/views/activities/home/MainActivity.java
@@ -3,20 +3,69 @@ package com.mobss.namesofAllah.views.activities.home;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.mobss.namesofAllah.R;
 import com.mobss.namesofAllah.views.activities.base.BaseActivity;
+import com.mobss.namesofAllah.views.widgets.dialogs.rateme.Config;
+import com.mobss.namesofAllah.views.widgets.dialogs.rateme.RateMe;
+
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity implements MainMvpView {
+	
+	@Inject
+	MainMvpPresenter<MainMvpView> mPresenter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		getActivityComponent().inject(this);
+		
+		setUnBinder(ButterKnife.bind(this));
+		
+		RateMe.init(new Config(5, 10)); // 5 gün ya da 10 defa uygulama başlattıktan sonra
+		
+		// Attach presenter
+		mPresenter.onAttach(MainActivity.this);
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		RateMe.onStart(this);
+		RateMe.showRateDialogIfNeeded(this);
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		mPresenter.onDetach();
+		super.onDestroy();
 	}
 	
 	public static Intent getStartIntent(Context context) {
 		Intent intent = new Intent(context, MainActivity.class);
 		return intent;
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			
+			finish();
+			
+			return true;
+		} else {
+			return super.onKeyDown(keyCode, event);
+		}
 	}
 }
